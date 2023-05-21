5a8d96e39e7fbedb6d19b8367f1ada91ef4e0ff3
Yemoe Aung <108300573+yemoeaung1@users.noreply.github.com>
2023-05-20 22:53:37 -0400

    both scores update after win

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const choices = {
     rock: 0,
     paper: 1,
@@ -9,23 +8,29 @@ let playerWin = 0;
 let compWin = 0;
 
 
+
+
 function getComputerChoice() {
-    return Math.floor(Math.random() * (2-0+1));
+    return Math.floor(Math.random() * (3));
 }
 
-function getPlayerChoice(playerPrompt) {
-    return choices[playerPrompt];
-}
+// function getPlayerChoice(playerPrompt) {
+//     return choices[playerPrompt];
+// }
 
 function determineWin(compChoice, playerChoice) {
     if(playerChoice > compChoice) {
         playerWin++;
-        return "player wins";
+        playerScore.textContent = playerWin;
+        roundInfo.textContent = "Player wins the round";
+        //return "player wins";
     } else if(playerChoice < compChoice) {
         compWin++;
+        compScore.textContent = compWin;
+        roundInfo.textContent = "Computer wins the round";
         return "computer wins";
     } else 
-        return "tied";
+        roundInfo.textContent = "Tie round";
 }
 
 function game() {
@@ -45,4 +50,28 @@ function game() {
     }
 }
 
-game();
+const playerScore = document.getElementById("player-score");
+const compScore = document.getElementById("cp-score");
+const roundInfo = document.getElementById("message");
+
+const rockDiv = document.getElementById("rock");
+const paperDiv = document.getElementById("paper");
+const sciDiv = document.getElementById("scissors");
+
+rockDiv.addEventListener("click", () => handle(0));
+paperDiv.addEventListener("click", () => handle(1));
+sciDiv.addEventListener("click", () => handle(2));
+
+function handle(playerOption) {
+    if(playerWin != 5 || compWin != 5) {
+        playerChoice = playerOption;
+        compChoice = getComputerChoice();
+        determineWin(compChoice, playerChoice);
+    } else {
+        roundInfo.textContent = "Game Over";
+        rockDiv.removeEventListener("click",() => handle(0));
+        paperDiv.removeEventListener('onclick',() => handle(1));
+        sciDiv.removeAttribute('onclick');
+    //playerScore.textContent = playerChoice;
+    }
+}
